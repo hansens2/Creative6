2690dddd53c88e8b5ebf2bbd66893e1ecd6d475b
Mitchell Foote <mitchellfoote93@gmail.com>
2017-03-28 16:54:08 -0600

    Added CSS file

--- Creative-Project-5/app.js
+++ Creative-Project-5/app.js
@@ -4,8 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-//var mongoose = require('mongoose');
-//mongoose.connect('mongodb://localhost/message');
+
 
 
 var routes = require('./routes/index');
